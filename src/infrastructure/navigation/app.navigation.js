d006a5af275e80364ad9ef5177ec9582f67b997b
Ugurcan Cetin <ugurcan.cetin@ericsson.com>
2021-03-31 23:54:43 +0200

    refactor for the navigation to clean up app.js structure

---
+++ src/infrastructure/navigation/app.navigation.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import { RestaurantsScreen } from "../../../src/features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "../../../src/components/safe-area.component";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
+const createScreenOptions = ({ route }) => {
+    const iconName = TAB_ICON[route.name];
+    return {
+      tabBarIcon: ({ size, color }) => (
+        <Ionicons name={iconName} size={size} color={color} />
+      ),
+    };
+  };
+
+  const Settings = () => (
+    <SafeArea>
+      <Text>Settings</Text>
+    </SafeArea>
+  );
+
+  const Map = () => (
+    <SafeArea>
+      <Text>Map</Text>
+    </SafeArea>
+  );
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <Tab.Navigator
+      screenOptions={createScreenOptions}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
